76d83d39358dd4dfe78422e085a56d53f0697272
Saishiva1502 <107388324+Saishiva1502@users.noreply.github.com>
2022-09-17 21:23:17 +0530

    Update ExampleInstrumentedTest.kt

--- GroceryApp-main (1)/GroceryApp-main/app/src/androidTest/java/com/saishivareddy/groceryapp/ExampleInstrumentedTest.kt
+++ GroceryApp-main (1)/GroceryApp-main/app/src/androidTest/java/com/saishivareddy/groceryapp/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.divyanshu.groceryapp
+package com.saishivareddy.groceryapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
